1025f0ed9c2d6f83383361bd7baf1176a4f3ccb1
f.siffer <f.siffer@gmail.com>
2019-05-14 13:31:43 +0200

    added movement controls

--- src/ReFrac.hs
+++ src/ReFrac.hs
@@ -3,16 +3,38 @@ module ReFrac where
 -- Type
 
 data ReFracState = ReFracState {
-    _zoom :: Float,
-    _x :: Float,
-    _y :: Float,
-    _frac :: Float -> Float -> Float
+    getZoom :: Float,
+    getX :: Float,
+    getY :: Float,
+    getFracF :: Float -> Float -> Float
 }
 
+data ZoomDirection
+    = In
+    | Out
+    deriving (Eq, Show)
 
--- Fractal
+data MoveDirection
+    = MDUp
+    | MDDown
+    | MDLeft
+    | MDRight
+    deriving (Eq, Show)
 
 initialState =  ReFracState 0.0 0.0 0.0 notFractal
+
+-- Navigation
+move :: MoveDirection -> ReFracState -> ReFracState
+move MDUp s = ReFracState (getZoom s) (getX s) (getY s - 1) (getFracF s)
+move MDDown s = ReFracState (getZoom s) (getX s) (getY s + 1) (getFracF s)
+move MDLeft s =  ReFracState (getZoom s) (getX s + 1) (getY s) (getFracF s)
+move MDRight s =  ReFracState (getZoom s) (getX s - 1) (getY s) (getFracF s)
+
+zoom :: ZoomDirection -> ReFracState -> ReFracState
+zoom In s = ReFracState (getZoom s + 1) (getX s) (getY s) (getFracF s)
+zoom Out s = ReFracState (getZoom s - 1) (getX s) (getY s) (getFracF s)
+
+-- Fractal
 
 notFractal :: Float -> Float -> Float
 notFractal _ _ = 0
--- src/UI.hs
+++ src/UI.hs
@@ -4,7 +4,7 @@ import Control.Monad (void)
 
 import ReFrac
 import Brick
-import Graphics.Vty (Event( EvKey ) , Key( KChar, KEsc ), defAttr)
+import Graphics.Vty (Event( EvKey ) , Key( KChar, KEsc, KUp, KDown, KRight, KLeft), defAttr)
 import Control.Monad.IO.Class (liftIO)
 
 -- Types
@@ -24,6 +24,12 @@ main = do
 
 -- Handling events
 handleEvent :: ReFracState -> BrickEvent () () -> EventM () (Next ReFracState)
+handleEvent s (VtyEvent (EvKey KUp []))         = continue $ move MDUp s
+handleEvent s (VtyEvent (EvKey KDown []))       = continue $ move MDDown s
+handleEvent s (VtyEvent (EvKey KRight []))      = continue $ move MDRight s
+handleEvent s (VtyEvent (EvKey KLeft []))       = continue $ move MDLeft s
+handleEvent s (VtyEvent (EvKey (KChar 'i') [])) = continue $ zoom In s
+handleEvent s (VtyEvent (EvKey (KChar 'o') [])) = continue $ zoom Out s
 handleEvent s (VtyEvent (EvKey (KChar 'r') [])) = continue initialState
 handleEvent s (VtyEvent (EvKey (KChar 'q') [])) = halt s
 handleEvent s (VtyEvent (EvKey KEsc []))        = halt s
@@ -31,7 +37,7 @@ handleEvent s _                                 = continue s
 
 -- Drawing
 drawUI :: ReFracState -> [Widget ()]
-drawUI _ = [str "Hello, world!"]
+drawUI s = [str ("Hello, world at " ++ show (getX s))]
 
 reFracAttrMap :: AttrMap
 reFracAttrMap = attrMap defAttr []
